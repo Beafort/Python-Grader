0334cadf8965ba7942032e940d79f1e03cf473c3
Vũ Trung Hiếu <Hieu.VT2416491@sis.hust.edu.vn>
2026-02-24 13:27:07 +0700

    refactor: move helpers function to utils.py

---
+++ utils.py
@@ -0,0 +1,75 @@
+
+def open_file(name):
+    try:
+        f = open("Data Files/" + name, "r")
+        print("Successfully opened " + name)
+        return f
+    except FileNotFoundError:
+        print("Error: File cannot be found")
+    except Exception as e:
+        print("Unexpected error:", e)
+        
+
+def check_student_id(id: str) -> bool: 
+    
+    if id[0] != 'N' or len(id) != 9:
+        return False
+    else: 
+        return True
+    
+        
+def analyze(content : str):
+    print("**** ANALYZING ***")
+    lines = content.splitlines()
+    invalid_count = 0
+    for line in lines: 
+        # error list 
+        errors: list[str] = []
+
+        data = line.split(",")
+        student_id = data[0]
+
+        # validity check
+        if not check_student_id(student_id):
+            errors.append("N# is invalid")
+
+        if len(data) != 26:
+            errors.append("line does not contain exactly 26 values")
+
+        #print errors if any
+        if len(errors) != 0:
+            invalid_count += 1
+            print("Invalid line of data:", end=" ")
+
+            for error in errors:
+                print(error, end=" | ")
+            print()
+            print(line)
+            print()
+            continue
+
+    if invalid_count == 0:
+        print("No errors found!")
+    
+    return lines, invalid_count
+
+
+def grade(ans: str, key: str):
+    ans_list = ans.split(",")
+    ans_list = ans_list[1:]
+    key_list = key.split(",")
+
+    grade = 0
+
+    for a, k in zip(ans_list, key_list):
+        if a == k:
+            grade += 4
+        elif a == "":
+            continue
+        else:
+            grade -= 1
+
+    return grade
+
+
+        
