45c00f2cdf6958f4809b72e55c81d77ce1b095ff
Vũ Trung Hiếu <Hieu.VT2416491@sis.hust.edu.vn>
2026-02-24 13:25:23 +0700

    feat: added grading function
    grading rules:
    corrrect = 4 points
    incorrect = -1 points
    empty = 0 points

--- Vu_Hieu_grade_the_exams.py
+++ Vu_Hieu_grade_the_exams.py
@@ -50,13 +50,34 @@ def analyze(content : str):
             continue
 
     if invalid_count == 0:
-        print("No errors found! :D")
+        print("No errors found!")
     
     return lines, invalid_count
 
 
+def grade(ans: str, key: str):
+    ans_list = ans.split(",")
+    ans_list = ans_list[1:]
+    key_list = key.split(",")
+
+    grade = 0
+
+    for a, k in zip(ans_list, key_list):
+        if a == k:
+            grade += 4
+        elif a == "":
+            continue
+        else:
+            grade -= 1
+
+    return grade
+
+
+        
 
 def main():
+    answer_key = "B,A,D,D,C,B,D,A,C,C,D,B,A,B,A,C,B,D,A,C,A,A,B,D,D"
+
     file_name = input("Enter file name: ")
     f = open_file(file_name)
     content = f.read()
@@ -64,8 +85,14 @@ def main():
     
     lines, invalid_count = analyze(content)
     
+    if invalid_count != 0:
+        return
 
-
+    grades = []
+    for line in lines:
+        grades.append(grade(line, answer_key))
+    print(grades)
+    
 
 
 if __name__ == "__main__":
